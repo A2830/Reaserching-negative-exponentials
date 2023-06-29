5d7906fd3ca2e945e2d66a82b3e94bf6099e59e3
A2830 <ammarsid22@gmail.com>
2023-06-29 16:03:08 -0400

    added input
    wdwe

--- Negative Exponentials/load.js
+++ Negative Exponentials/load.js
@@ -1,1 +1,8 @@
-console.log('Main')
+let name = document.getElementById('name');
+let equation = document.getElementById('equate');
+function load(){
+    console.log('loading');
+   
+    console.log('Name: '+name+' Equation: '+equation);
+    
+}
