72af91131f02e960edc1b8e7068453e7426326c3
A2830 <ammarsid22@gmail.com>
2023-06-30 15:10:04 -0400

    Added console functionality
    Equations recognized, but required does not work

--- Negative Exponentials/load.js
+++ Negative Exponentials/load.js
@@ -1,8 +1,33 @@
-let name = document.getElementById('name');
-let equation = document.getElementById('equate');
+
 function load(){
-    console.log('loading');
-   
-    console.log('Name: '+name+' Equation: '+equation);
+    // get inputs
+    var name = document.getElementById('name').value;
+    var aValue = document.getElementById('equateA').value;
+    var bValue = document.getElementById('equateB').value;
+    var hValue = document.getElementById('equateH').value;
+    var kValue = document.getElementById('equateK').value;
+    //console debug
+        //set h and k for console
+    if(hValue >= 0){
+        var hValueCons = '-' + hValue.toString();
+
+    } else{
+        var hValueCons = '+'+ hValue.toString();
+    }
+    if(kValue >= 0 ){
+        var kValueCons = '+' + kValue.toString();
+
+    } else{
+        var kValueCons = '-'+ kValue.toString();
+    }
+        // show values
+    console.log('Name: '+name+' Equation: '+aValue.toString()+'(-'+bValue.toString()+')^(x'+hValueCons+')'+kValueCons);
+        // end Debug r1
+    //change title
+    titleProject = document.getElementById('nameOf');
+    titleProject.innerHTML = name;
+
+
+    
     
 }
