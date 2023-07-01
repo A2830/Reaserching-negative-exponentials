740bf4d1960598cc23a0b48eba11c8f8c0a30df6
A2830 <ammarsid22@gmail.com>
2023-06-30 21:22:45 -0400

    Added basic plug in

--- Negative Exponentials/load.js
+++ Negative Exponentials/load.js
@@ -6,6 +6,8 @@ function load(){
     var bValue = document.getElementById('equateB').value;
     var hValue = document.getElementById('equateH').value;
     var kValue = document.getElementById('equateK').value;
+    var rMin = document.getElementById('minRange').value;
+    var rMax = document.getElementById('maxRange').value;
     //console debug
         //set h and k for console
     if(hValue >= 0){
@@ -21,14 +23,32 @@ function load(){
         var kValueCons = '-'+ kValue.toString();
     }
         // show values
-    console.log('Name: '+name+' Equation: '+aValue.toString()+'(-'+bValue.toString()+')^(x'+hValueCons+')'+kValueCons);
+    console.log('Name: '+name+' Equation: '+aValue.toString()+'(-'+bValue.toString()+')^(x'+hValueCons+')'+kValueCons+' Range: ['+rMin+','+rMax+']');
         // end Debug r1
     //change title
     titleProject = document.getElementById('nameOf');
     titleProject.innerHTML = name;
-    //get table
+    //get points
     function tablePoints(){
-        
+        // set min range as first point
+        var i = rMin;
+        var index = i;
+        // repaets til index reaches max range
+        //plug in step by step and log for debug
+        while (index < rMax){
+            var pointX = index;
+            console.log('xValue:'+pointX);
+            var exponentMkr = index + hValue;
+            console.log('raisedTo:'+exponentMkr);
+            var assembler = a*Math.pow((-1*bValue),exponentMkr);
+            console.log('assembleMultiplication:'+assembler);
+            var pointY = assembler + kValue;
+            //display
+
+            
+            
+
+        }
 
     }
 
